249d870a7349d4c099a527d1491c2d8b0849e165
Wally Faye <wally.faye@gmail.com>
2017-07-18 23:22:41 -0400

    colors

--- _Demo.java
+++ _Demo.java
@@ -7,12 +7,27 @@ class _Demo {
 		int height = 4;
 		int width = 4;
 		int[][] matrix = new int[height][width];
+
+		int[] colors = new int[8];
+		colors[0] = 40;
+		colors[1] = 41;
+		colors[2] = 42;
+		colors[3] = 43;
+		colors[4] = 44;
+		colors[5] = 45;
+		colors[6] = 46;
+		colors[7] = 47;
+		System.out.println("");
 		for(int h = 0; h < height; h++){
+			String row_out = "";
 			for(int w = 0; w < width; w++){
 				int val = h + w;
-				System.out.println(h + ", " + w + ", " + val);
 				matrix[h][w] = val;
+//				row_out += val + " ";
+				row_out += (char)27 + "[" + colors[val] + "m " + val + " "; 
 			}
+			System.out.println(row_out);
 		}
+		System.out.println((char)27 + "[0m");
 	}
 }
