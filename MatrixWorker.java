2d55e503997683c7fd4125d6dee955c3bd80f939
Wally Faye <wally.faye@gmail.com>
2017-07-25 21:31:53 -0400

    rotate setups

--- MatrixWorker.java
+++ MatrixWorker.java
@@ -1,36 +1,44 @@
 class MatrixWorker{
+
 	private int[][] matrix2d;
 	private int matrix_size;
+        private int[] colors = new int[]{40, 41, 42, 43, 44, 45, 46, 47};
+	private int height = matrix_size;
+	private int width = matrix_size;
+
 	public MatrixWorker(int matrix_size){
 		this.matrix_size = matrix_size;
+		this.height = this.matrix_size;
+		this.width = this.matrix_size;
 		this.matrix2d = new int[matrix_size][matrix_size];
 	}
-	public void print(){
-                int height = this.matrix_size;
-                int width = this.matrix_size;
-                int[][] matrix = new int[height][width];
 
-                int[] colors = new int[8];
-                colors[0] = 40;
-                colors[1] = 41;
-                colors[2] = 42;
-                colors[3] = 43;
-                colors[4] = 44;
-                colors[5] = 45;
-                colors[6] = 46;
-                colors[7] = 47;
+	public void rotate(){
+		
+	}
+
+	public void print(){
+
                 System.out.println("");
-                for(int h = 0; h < height; h++){
+
+                for(int h = 0; h < this.height; h++){
+
                         String row_out = "";
-                        for(int w = 0; w < width; w++){
+
+                        for(int w = 0; w < this.width; w++){
+
                                 int val = h + w;
                                 this.matrix2d[h][w] = val;
-//                              row_out += val + " ";
-                                row_out += (char)27 + "[" + colors[val] + "m " + val + " ";
+                                row_out += (char)27 + "[" + this.colors[val] + "m " + val + " ";
+
                         }
+
                         System.out.println(row_out);
+
                 }
+
                 System.out.println((char)27 + "[0m");
+
 	}
 
 }
--- _Demo.java
+++ _Demo.java
@@ -1,11 +1,21 @@
 class _Demo {
 	public static void main(String[] args){
-		int matrix_size = Integer.parseInt(args[0]);
-		if(args.length != 1 || matrix_size > 4 || matrix_size < 1){
+
+		if(args.length != 1){
 			System.out.println("You must enter a number from 1 - 4");
 			System.exit(0);
 		}
+
+		int matrix_size = Integer.parseInt(args[0]);
+		if(matrix_size > 4 || matrix_size < 1){
+			System.out.println("You must enter a number from 1 - 4");
+			System.exit(0);
+		}
+
 		MatrixWorker mworker = new MatrixWorker(matrix_size);
 		mworker.print();
+		mworker.rotate();
+		mworker.print();
+
 	}
 }
